a4a48e8b9a8988a17c38bd63675f37bd70eda945
Oleksii Skidan <Oleksii.Skidan@ua.ibm.com>
2022-04-11 13:09:31 +0300

    Cleanup the code a bit. Add game_state.
    This refactorring is happening too early, to my taste. But as
    I'm not going to add new functinoality into the "game", let's
    cleanup the code...

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
-#include "jam/physics/mass.hpp"
-#include <chrono>
+#include <algorithm>
 #include <span>
 
 #include <fmt/format.h>
@@ -13,90 +12,223 @@
 #include <ftxui/component/event.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 
-void draw_line(ftxui::Canvas &c, jam::point const p0, jam::point const p1)
+using namespace jam::literals;
+using namespace std::string_literals;
+
+//=======================================================================================================================
+namespace {
+
+template<typename T> constexpr inline T canvas_width = T{ 160 };
+template<typename T> constexpr inline T canvas_height = T{ 120 };
+
+
+constexpr inline jam::force gravity{ 0_F, 9.8_F };
+
+//----------------------------------------------------------------------------------------------------------------------
+
+struct game_state
+{
+  game_state() { trail_.reserve(maximum_trail_size); }
+
+  void draw_on(ftxui::Canvas &canvas);
+
+  void update(ftxui::Event &event);
+
+private:
+  jam::point mouse_position_{ 0_F, 0_F };
+
+  bool mouse_clicked_{ false };
+
+  bool picked_{ false };
+
+  std::size_t score_{ 0 };
+
+  std::vector<jam::particle> thing_{
+    jam::particle{ jam::point{ 10_F, 10_F }, jam::infinite_mass },// NOLINT TODO: load these from a file
+    jam::particle{ jam::point{ 20_F, 10_F }, 0.1_kg },// NOLINT TODO: load these from a file
+    jam::particle{ jam::point{ 20_F, 20_F }, 0.1_kg },// NOLINT TODO: load these from a file
+    jam::particle{ jam::point{ 10_F, 20_F }, 0.1_kg },// NOLINT TODO: load these from a file
+  };
+
+  constexpr static inline auto spring_constant = 19_F;
+  std::vector<jam::spring_constraint> thing_contraints_{
+    jam::spring_constraint{ thing_[0], thing_[1], spring_constant },
+    jam::spring_constraint{ thing_[1], thing_[2], spring_constant },
+    jam::spring_constraint{ thing_[2], thing_[3], spring_constant },
+    jam::spring_constraint{ thing_[3], thing_[0], spring_constant },
+    // Diagonals:
+    jam::spring_constraint{ thing_[0], thing_[2], spring_constant },
+    jam::spring_constraint{ thing_[1], thing_[3], spring_constant },
+  };
+
+  // TODO: use the `z` suffix once the code base is updated to C++23.
+  constexpr static inline std::size_t maximum_trail_size = 200;
+  std::vector<jam::particle> trail_;
+
+  jam::clock clock_{};
+
+  void handle_mouse(ftxui::Event &event);
+
+  void tick(jam::time dt);
+
+  void collect_trail_garbage();
+
+  void pick_the_thing();
+
+  void update_the_score();
+
+  void spawn_trail_particles();
+
+  void update_physics(jam::time dt);
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void draw_line(ftxui::Canvas &c, jam::point const p0, jam::point const p1, ftxui::Color color = ftxui::Color::Red)
 {
   auto const p0x = static_cast<float>(p0.x());
   auto const p0y = static_cast<float>(p0.y());
   auto const p1x = static_cast<float>(p1.x());
   auto const p1y = static_cast<float>(p1.y());
-  c.DrawPointLine(
-    static_cast<int>(p0x), static_cast<int>(p0y), static_cast<int>(p1x), static_cast<int>(p1y), ftxui::Color::Red);
+  c.DrawPointLine(static_cast<int>(p0x), static_cast<int>(p0y), static_cast<int>(p1x), static_cast<int>(p1y), color);
 }
 
-void draw_shape(ftxui::Canvas &c, std::span<jam::particle> const particles)
+void draw_shape(ftxui::Canvas &c, std::span<jam::particle const> const particles)
 {
   auto const n = particles.size();
   for (std::size_t i = 0; i < n; ++i) { draw_line(c, particles[i].position(), particles[(i + 1) % n].position()); }
 }
 
+void draw_text(ftxui::Canvas &c, jam::point const &text_position, std::string const &text)
+{
+  auto const text_x = static_cast<float>(text_position.x());
+  auto const text_y = static_cast<float>(text_position.y());
+  c.DrawText(static_cast<int>(text_x), static_cast<int>(text_y), text);
+}
+
+void draw_point(ftxui::Canvas &c, jam::point const &position)
+{
+  auto const x = static_cast<float>(position.x());
+  auto const y = static_cast<float>(position.y());
+  c.DrawPointOn(static_cast<int>(x), static_cast<int>(y));
+}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+void game_state::draw_on(ftxui::Canvas &c)
+{
+  c.DrawText(canvas_width<int> / 2, canvas_height<int> - 1, fmt::format("Score: {}", score_));
+
+  for (auto const &trail_particle : trail_) { draw_point(c, trail_particle.position()); }
+
+  draw_shape(c, thing_);
+
+  if (picked_) {
+    static const auto picked_label = "Drag me aROUND!"s;
+    draw_text(c, mouse_position_, picked_label);
+  } else {
+    draw_line(c, thing_[0].position(), mouse_position_, ftxui::Color::Blue);
+    static const auto hint_label = "Pick me!"s;
+    draw_text(c, mouse_position_, hint_label);
+  }
+}
+
+void game_state::update(ftxui::Event &event)
+{
+  if (event.is_mouse()) {
+    handle_mouse(event);
+  } else {
+    tick(clock_.update());
+  }
+}
+
+void game_state::handle_mouse(ftxui::Event &event)
+{
+  mouse_position_ = jam::point{
+    jam::scalar{ static_cast<float>(event.mouse().x * 2) },
+    jam::scalar{ static_cast<float>(event.mouse().y * 4) },
+  };
+  mouse_clicked_ = event.mouse().button == ftxui::Mouse::Left;
+}
+
+void game_state::tick(jam::time const dt)
+{
+  collect_trail_garbage();
+  pick_the_thing();
+  update_the_score();
+  spawn_trail_particles();
+  update_physics(dt);
+}
+
+void game_state::collect_trail_garbage()
+{
+  if (trail_.size() == maximum_trail_size) {
+    auto const first_particle = begin(trail_);
+    auto const sentinel = end(trail_);
+
+    trail_.erase(remove_if(first_particle,
+                   sentinel,
+                   [boundary_of_doom = canvas_height<jam::scalar> - 1_F](
+                     jam::particle const &particle) { return particle.y() > boundary_of_doom; }),
+      sentinel);
+  }
+}
+
+void game_state::pick_the_thing()
+{
+  constexpr auto picking_radius = 8_F;
+  picked_ = length(mouse_position_ - thing_[0].position()) < picking_radius && mouse_clicked_;
+  if (picked_) { thing_[0].move_to(mouse_position_); }
+}
+
+void game_state::update_the_score()
+{
+  if (picked_) { ++score_; }
+}
+
+void game_state::spawn_trail_particles()
+{
+  constexpr auto score_100 = std::size_t{ 100 };
+  if (picked_ && trail_.size() < maximum_trail_size && (score_ % score_100) == std::size_t{ 0 }) {
+    trail_.emplace_back(mouse_position_, 0.1_kg);// NOLINT This magical number is self explanatory.
+  }
+}
+
+void game_state::update_physics(jam::time const dt)
+{
+  for (auto &p : thing_) { p.apply(gravity); }
+  for (auto &s : thing_contraints_) { s.resolve_constraint(); }
+  for (auto &p : thing_) { p.update(dt); }
+
+  for (auto &p : trail_) { p.apply(gravity); }
+  for (auto &p : trail_) { p.update(dt); }
+}
+
+}// namespace
+
+//======================================================================================================================
+
 int main()
 {
-  int mouse_x = 0;
-  int mouse_y = 0;
+  game_state game{};
 
-  using namespace jam::literals;
-
-  std::vector<jam::particle> particles;
-  particles.emplace_back(jam::point{ 10_F, 10_F }, jam::infinite_mass);// NOLINT
-  particles.emplace_back(jam::point{ 20_F, 10_F }, 0.1_kg);// NOLINT
-  particles.emplace_back(jam::point{ 20_F, 20_F }, 0.1_kg);// NOLINT
-  particles.emplace_back(jam::point{ 10_F, 20_F }, 0.1_kg);// NOLINT
-  std::vector<jam::spring_constraint> springs;
-  springs.emplace_back(particles[0], particles[1], 19_F);// NOLINT
-  springs.emplace_back(particles[1], particles[2], 19_F);// NOLINT
-  springs.emplace_back(particles[2], particles[3], 19_F);// NOLINT
-  springs.emplace_back(particles[3], particles[0], 19_F);// NOLINT
-  // Diagonals:
-  springs.emplace_back(particles[0], particles[2], 19_F);// NOLINT
-  springs.emplace_back(particles[1], particles[3], 19_F);// NOLINT
-
-  bool picked = false;
-
-  jam::force gravity{ 0_F, 9.8_F };// NOLINT
+  // NOTE: not going to add it into the game_state yet.
 
   auto screen = ftxui::ScreenInteractive::FitComponent();
 
   auto const renderer = ftxui::Renderer([&] {
-    constexpr int canvas_width = 160;
-    constexpr int canvas_height = 120;
+    auto c = ftxui::Canvas(canvas_width<int>, canvas_height<int>);
+    game.draw_on(c);
 
-    auto c = ftxui::Canvas(canvas_width, canvas_height);
-
-    draw_shape(c, particles);
-    if (picked) { c.DrawText(mouse_x, mouse_y, "Picked"); }
-
+    // Request new frame ASAP.
     screen.PostEvent(ftxui::Event::Custom);
 
     return canvas(std::move(c));
   });
 
-  jam::clock clock;
 
   auto const game_loop = ftxui::CatchEvent(renderer, [&](ftxui::Event e) {
-    if (e.is_mouse()) {
-      picked = false;
-
-      mouse_x = e.mouse().x * 2;
-      mouse_y = e.mouse().y * 4;
-
-      if (e.mouse().button == ftxui::Mouse::Left) {
-        auto const particle_x = static_cast<float>(mouse_x);
-        auto const particle_y = static_cast<float>(mouse_y);
-        jam::point mouse_point{ jam::scalar{ particle_x }, jam::scalar{ particle_y } };
-        constexpr auto picking_radius = 8_F;
-        picked = length(mouse_point - particles[0].position()) < picking_radius;
-        if (picked) { particles[0].move_to(jam::scalar{ particle_x }, jam::scalar{ particle_y }); }
-      }
-    }
-
-
-    if (e == ftxui::Event::Custom) {
-      auto const dt = clock.update();
-      for (auto &p : particles) { p.apply(gravity); }
-      for (auto &s : springs) { s.resolve_constraint(); }
-      for (auto &p : particles) { p.update(dt); }
-    }
-
+    game.update(e);
     return false;
   });
 
